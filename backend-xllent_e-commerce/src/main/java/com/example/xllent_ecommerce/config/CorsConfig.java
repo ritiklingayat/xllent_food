617940cd1b1d6cb835050239ac5b444dd48add35
Admin <Admin@DESKTOP-HGD186A>
2026-08-05 12:55:33 +0530

    update url

--- backend-xllent_e-commerce/src/main/java/com/example/xllent_ecommerce/config/CorsConfig.java
+++ backend-xllent_e-commerce/src/main/java/com/example/xllent_ecommerce/config/CorsConfig.java
@@ -24,7 +24,8 @@ public class CorsConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         frontendUrl,
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://www.xllentfoods.in"
                 )
         );
 
